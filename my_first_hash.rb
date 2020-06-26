0f9e6e56225ef48e9cb575c758a77dc24fbe4b25
Hope Gipson <gipson.hope@gmail.com>
2020-06-26 02:52:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   my_hash { 
-    "Chiefs QB" => "Patrick Mahomes"
-    "Chiefs TE" => "Travis Kelce"
+    "Chiefs QB" => "Patrick Mahomes",
+    "Chiefs TE" => "Travis Kelce",
     "Chiefs D" => "Tyrann Mathieu"
   }
 
